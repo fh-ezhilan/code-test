9758b722d34c9ee28222a1690e5a1c0eb2dd1d23
fh-ezhilan <ezhilan.b@foodhub.com>
2025-12-10 15:10:14 +0530

    integrated judge0 for compilation

--- server/controllers/candidateController.js
+++ server/controllers/candidateController.js
@@ -161,9 +161,7 @@ exports.runCode = async (req, res) => {
       },
       {
         headers: {
-          'Content-Type': 'application/json',
-          'X-RapidAPI-Key': process.env.JUDGE0_API_KEY || '',
-          'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+          'Content-Type': 'application/json'
         }
       }
     );
